8e26a4cdd7aae0dde5febf0568cca398a867808f
Natallia Shliapik <nshliapik@gmail,com>
2023-08-01 23:36:23 +0300

    build

--- test-task/src/CONST.js
+++
@@ -1,6 +0,0 @@
-export const charactersFetchURL = {
-  characters: 'https://rickandmortyapi.com/api/character',
-  locations: 'https://rickandmortyapi.com/api/location',
-  episodes: 'https://rickandmortyapi.com/api/episode',
-  charactersPage: 'https://rickandmortyapi.com/api/character/?page=',
-};
--- test-task/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-
-import charactersReducer from './charactersSlice';
-
-export const store = configureStore({
-  reducer: {
-    characters: charactersReducer,
-  },
-});
