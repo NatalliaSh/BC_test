d2eb32d090c4c0c4f555d25615470207590ed052
Natallia Shliapik <nshliapik@gmail,com>
2023-08-01 22:38:40 +0300

    change loader

--- test-task/src/components/Loader.js
+++ test-task/src/components/Loader.js
@@ -2,5 +2,14 @@ import React from 'react';
 import './Loader.scss';
 
 export const Loader = () => {
-  return <div className='Loader' />;
+  return (
+    <div className='Loader'>
+      <div className='spinner'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
